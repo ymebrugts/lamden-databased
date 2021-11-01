7a346b057c7d52cc9ee9aed250f325612cfe7983
Yme Brugts <yme@resolutionsbv.nl>
2021-11-01 21:44:13 +0100

    added config files

---
+++ databased-app/.prettierrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+	"vetur.format.defaultFormatterOptions": {
+		"js-beautify-html": {
+			"wrap_attributes": "auto",
+			"trimTrailingWhiteSpace": false
+
+		},
+		"prettyhtml": {
+			"wrapAttributes": true,
+			"printWidth": 200,
+			"trimTrailingWhiteSpace": false
+		},
+		"prettier": {
+			"semi": false,
+			"singleQuote": true,
+			"eslintIntegration": true,
+			"trailingComma": "none",
+			"printWidth": 200,
+			"trimTrailingWhiteSpace": false
+		}
+	},
+	"printWidth": 200
+}
