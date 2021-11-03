de014e544f0fb4a8a64e14ce876bce47461ebddb
Roy van Dijk <roy.van.dijk@outlook.com>
2021-11-03 20:36:50 +0100

    calculator added

--- databased-app/src/App.vue
+++ databased-app/src/App.vue
@@ -1,30 +1,28 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import url('https://use.typekit.net/cgv3imf.css');
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+:root {
+  --purple: #8c20ba;
+}
+
+html, body {
+  font-family: sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  color: #aaa;
+  background-color: #000;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- databased-app/src/views/Home.vue
+++ databased-app/src/views/Home.vue
@@ -1,18 +1,166 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="wrapper">
+    <header class="header">
+      <button class="button">CONNECT LAMDEN WALLET</button>
+    </header>
+    <img alt="Databased logo" src="../assets/logo.png" class="logo" />
+    <LogoText />
+    <p class="subtitle">
+      <span>BASED</span> is a community currency built on Lamden.
+    </p>
+    <p class="slogan">
+      BUY | STAKE | FARM
+    </p>
+    <h2 class="subheading">
+      What will your assets <br class="break"> be worth?<span>*</span>
+    </h2>
+    <p class="subheading-subtitle"> 
+      Calculate The Value And MC Of Your Lamden Assets At Any Price.
+    </p>
+
+    <Calculator />
+    
+    <footer>
+      <p class="slogan">
+        LOCKED LP | NO TEAM TOKENS
+      </p>
+
+      <p class="distribution">
+        <span class="underline">DISTRIBUTION:</span><br>
+        25% Telegram Airdrop (Complete)<br>
+        25% Initial Liquidity (To Be Locked On Nebula)<br>
+        25% BASED LP Farm (On Rocketswap)<br>
+        25% Yield Vaults (On Nebula)
+      </p>
+
+      <p>
+        *This Is For Entertainment Purposes Only And Does Not Constitute Financial Advice.
+      </p>
+    </footer>
+    
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Calculator from "@/components/Calculator.vue";
+import LogoText from "@/components/LogoText.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld,
+    Calculator,
+    LogoText
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.wrapper {
+  width: 100%;
+  max-width: 625px;
+  margin: 0 auto;
+  text-align: center;
+  font-family: sans-serif;
+  padding: 10px;
+  font-size: 0.98rem;
+}
+
+p {
+  margin: 10px 0;
+}
+
+.header {
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 10px;
+}
+
+.button {
+  background-color: var(--purple);
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  padding: 12px;
+  border-radius: 3px;
+  text-transform: uppercase;
+  letter-spacing: 0.5px;
+}
+
+.logo {
+  width: 100%;
+  height: 100%;
+  max-width: 120px;
+  display: block;
+  margin: 0 auto;
+}
+
+.subtitle {
+  font-size: 0.9rem;
+  letter-spacing: 0.8px;
+
+  span {
+    text-transform: uppercase;
+    color: var(--purple);
+  }
+}
+
+.slogan {
+  text-transform: uppercase;
+  color: var(--purple);
+  font-size: 1.2rem;
+  margin: 30px 0;
+}
+
+.subheading {
+  color: #fff;
+  line-height: 0.95rem;
+  margin-bottom: 10px;
+
+  span {
+    font-size: 0.8rem;
+    display: inline-block;
+    margin-left: 2px;
+  }
+}
+
+.subheading-subtitle {
+  text-transform: capitalize;
+}
+
+footer {
+  margin-top: 100px;
+
+  .distribution {
+    font-size: 0.95rem;
+    line-height: 1.3rem;
+    .underline {
+      text-transform: uppercase;
+      text-decoration: underline;
+    }
+  }
+}
+
+.break {
+  display: none;
+}
+@media (max-width: 800px) {
+  .subheading {
+    line-height: unset;
+  }
+
+  .break {
+    display: block;
+  }
+
+  .button { 
+    background-image: url('../assets/lamden.svg');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 70%;
+    font-size: 0;
+    width: 40px;
+    height: 40px;
+  }
+}
+</style>
